07a4344b81dc73704d368b3349b8ddf7ed81b1e4
akari <akariavatar@gmail.com>
2024-08-09 19:19:06 +0800

    发布页面布局更新

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 [moments](https://github.com/kingwrcy/moments) IOS版本,实在是太喜欢这个极简单朋友圈.写着玩...功能摸索中...
 
 ![Untitled.gif](https://raw.githubusercontent.com/akarikun/moments-ios/main/imgs/Untitled.gif)
+![post.gif](https://raw.githubusercontent.com/akarikun/moments-ios/main/imgs/post.gif)
--- moments-ios/PostVC.swift
+++ moments-ios/PostVC.swift
@@ -24,8 +24,8 @@ class PostVC: UIViewController,UITextFieldDelegate, PHPickerViewControllerDelega
         backImageView.isUserInteractionEnabled = true
         backImageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(back_handleTap)))
         
-        sendView.layer.cornerRadius = 3 // 设置圆角半径为 10
-        sendView.clipsToBounds = true // 确保子视图不会超出父视图的圆角边界
+        sendView.layer.cornerRadius = 3
+        sendView.clipsToBounds = true
         sendView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(send_handleTap)))
         
         pmsSwitch.addAction(UIAction(handler: switch_handleChanged), for: UIControl.Event.valueChanged)
@@ -35,14 +35,79 @@ class PostVC: UIViewController,UITextFieldDelegate, PHPickerViewControllerDelega
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
         pstText.delegate = self
         
-        
-        imageContainerView.subviews.first?.setBorder(UIColor.red)
-        imageContainerView.subviews.first?.isUserInteractionEnabled = true
-        imageContainerView.subviews.first?.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(imagelist_handleTap)))
+        setImageContainerLayout(imageContainerView)
+        appendImageView(imageContainerView,nil)
     }
+    
+    func setImageContainerLayout(_ imageContainer:UIView){
+        let width = UIScreen.main.bounds.size.width - 20
+        let widthConstraint = imageContainer.constraints.first(where: { $0.firstAttribute == .width })!
+        let heightConstraint = imageContainer.constraints.first(where: { $0.firstAttribute == .height })!
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            widthConstraint.constant = width / 2
+            //居左
+            imageContainer.superview!.constraints.first(where: { $0.firstAttribute == .leading && $0.firstItem as? UIView == imageContainer && $0.secondItem as? UIView == imageContainer.superview })!.priority = UILayoutPriority(1000)
+        }
+        else{
+            widthConstraint.constant = width
+            //居中
+            imageContainer.superview!.constraints.first(where: { $0.firstAttribute == .centerX && $0.firstItem as? UIView == imageContainer })!.priority = UILayoutPriority(1000)
+        }
+        var count = imageContainer.subviews.count + 1
+        let val:CGFloat = count > 6 ? 0 : count > 3 ? CGFloat(20 / 3) : 20
+        heightConstraint.constant = (width - val) * ceil(Double(count) / 3.0) * (1 / 3)
+        heightConstraint.priority = UILayoutPriority(1000)
+    }
+
+    func appendImageView(_ imageContainer:UIView,_ image:UIImage?){
+        setImageContainerLayout(imageContainer)
+        let width = imageContainer.constraints.first(where: { $0.firstAttribute == .width })!.constant
+        let count = imageContainer.subviews.count
+        let w = (width - 20) / 3
+        let r:Int = count / 3
+        let c:Int = count % 3
+        let row:CGFloat = CGFloat(r) * w + CGFloat(10 * r)
+        let col:CGFloat = CGFloat(c) * w + CGFloat(10 * c)
+        
+        let imageView = UIImageView(image: image == nil ? UIImage(named: "CarbonAddLarge") : image)
+        imageContainerView.addSubview(imageView)
+        imageView.frame = CGRect(x: col, y: row, width: w, height: w)
+        imageView.tag = image == nil ? -1 : 0
+        imageView.isUserInteractionEnabled = true
+        imageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(addImage_handleTap)))
+        changeImageView(imageContainer)
+    }
+    func changeImageView(_ imageContainer:UIView){
+        let count = imageContainer.subviews.count
+        if count <= 1{
+            return
+        }
+        let addImage = imageContainer.subviews.first { $0.tag == -1 } as! UIImageView
+        let last = imageContainer.subviews.last as! UIImageView
+        let t1 = last.frame
+        let t2 = addImage.frame
+        last.frame = CGRect(x: t2.minX, y: t2.minY, width: t2.width, height: t2.height)
+        addImage.frame = CGRect(x: t1.minX, y: t1.minY, width: t1.width, height: t1.height)
+    }
+    
+    @objc func addImage_handleTap(gesture: UITapGestureRecognizer){
+        print("addImage_handleTap")
+        var configuration = PHPickerConfiguration()
+//        configuration.filter = .images
+        configuration.selectionLimit = 9
+        let imagePicker = PHPickerViewController(configuration: configuration)
+        imagePicker.delegate = self
+        present(imagePicker, animated: true)
+    }
+    
     func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
         picker.dismiss(animated: true)
         let itemProviders = results.map(\.itemProvider)
+        
+        if self.imageContainerView!.subviews.count + results.count > 9{
+            print("不能超过9张")
+            return
+        }
         
         itemProviders.forEach {
             guard let typeIdentifier = $0.registeredTypeIdentifiers.first,
@@ -55,37 +120,12 @@ class PostVC: UIViewController,UITextFieldDelegate, PHPickerViewControllerDelega
             
             if utType.conforms(to: .image) {
                 $0.loadDataRepresentation(forTypeIdentifier: secondTypeIdentifier ?? typeIdentifier) { [weak self] data, error in
-                    let image = UIImage(data: data!)
                     DispatchQueue.main.async {
-                        let imageView = self?.imageContainerView.subviews.first as! UIImageView
-                        imageView.image = image
-                        self!.appendImageView()
+                        self?.appendImageView((self?.imageContainerView)!,UIImage(data: data!)!)
                     }
                 }
             }
         }
-    }
-    
-    func appendImageView(){
-        let lastView = imageContainerView.subviews.last as! UIImageView
-        let imageView = UIImageView(image: UIImage(named: "CarbonAddLarge"))
-        imageContainerView.addSubview(imageView)
-        imageView.setBorder(UIColor.orange)
-        imageView.snp.makeConstraints { make in
-            make.width.equalTo(lastView)
-            make.height.equalTo(lastView)
-            make.top.equalTo(0)
-            make.leading.equalTo(lastView.frame.width)
-        }
-    }
-
-    @objc func imagelist_handleTap(imageView: UIImageView){
-        var configuration = PHPickerConfiguration()
-//        configuration.filter = .images
-        configuration.selectionLimit = 9
-        let imagePicker = PHPickerViewController(configuration: configuration)
-        imagePicker.delegate = self
-        present(imagePicker, animated: true)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
